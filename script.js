09ef195a6450559a423d1d93f8657b366d108943
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2020-01-24 17:35:27 -0800

    Update

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+//mobile nav show and hide functions
+
 function showHideNav() {
-    if (document.getElementById("dropdown").style.display === "none") {
-        document.getElementById("dropdown").style.display = "flex";
+    if (document.getElementById('dropdown').style.display === 'none') {
+        document.getElementById('dropdown').style.display = 'flex';
     } else {
-        document.getElementById("dropdown").style.display = "none";
-        document.getElementById("sub-dropdown").style.display = "none";
+        document.getElementById('dropdown').style.display = 'none';
+        document.getElementById('sub-dropdown').style.display = 'none';
     }
 }
 function showHideSubNav() {
-    if (document.getElementById("sub-dropdown").style.display === "none") {
-        document.getElementById("sub-dropdown").style.display = "initial";
+    if (document.getElementById('sub-dropdown').style.display === 'none') {
+        document.getElementById('sub-dropdown').style.display = 'initial';
     } else {
-        document.getElementById("sub-dropdown").style.display = "none";
+        document.getElementById('sub-dropdown').style.display = 'none';
     }
+}
+
+//gallery functions
+function focusImg1() {
+    //document.getElementById('body').style.position = 'fixed'; 
+    document.getElementById('gallery-focus').style.display = 'initial';
+    document.getElementById('gallery-img1').style.position = 'absolute';
+    document.getElementById('gallery-img1').style.width = '80%';
+    document.getElementById('gallery-img1').style.top = '20vh';
+    document.getElementById('gallery-img1').style.zIndex = '2';
 }
