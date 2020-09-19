464098a7097acdeaa44347716b5dd84678751241
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2020-09-18 20:48:46 -0700

    We fucked up...

--- include/footer.php
+++ include/footer.php
@@ -1,16 +1,9 @@
-		<footer>
-			<div class="copyright">
-				<img src="images/logo.png" alt="Rainier Terrariums" class="footer-logo">
-				<h3>Copyright &copy; 2020 Rainier Terraiums</h3>
-			</div>
+        <footer>
+			<img src="images/logo.png" alt="Rainier Terrariums" class="footer-logo">
+			<h3 style="margin: 0; padding-top: 40px;">&copy; 2019 Kelson Milewski & Ryan Schlosser</h3>
 			<div class="footer-social-section">
-				<a href="https://discord.gg/eybXTrr" target="_blank"><img src="images/discord-logo.png" alt="Discord" class="footer-social"></a>
 				<a href="https://www.instagram.com/rainier_terrariums/" target="_blank"><img src="images/instagram-logo.png" target="_blank" alt="Instagram" class="footer-social"></a>
 				<a href="https://twitter.com/rterrariums" target="_blank"><img src="images/twitter-logo.png" target="_blank" alt="Twitter" class="footer-social"></a>
 				<a href="https://www.facebook.com/rainier.terrariums" target="_blank"><img src="images/facebook-logo.png" target="_blank" alt="Facebook" class="footer-social"></a>
 			</div>
-			<div class="footer-links">
-				<a href="contact.php">Contact Us</a>
-			</div>
 		</footer>
-		<script src="script.js"></script>
--- include/navbar.php
+++ include/navbar.php
@@ -1,22 +1,23 @@
-		<div class="navbar">
+        <div class="navbar">
 			<img src="images/text-logo.png" alt="Rainier Terrariums" class="nav-text-logo">
 			<img src="images/logo.png" alt="Rainier Terrariums" class="nav-logo">
 
 			<nav class="expand-navbar">
 				<ul>
-					<a href="home"><li>Home</li></a>
+					<li><a href="index.php">Home</a></li>
 					<li class="nav-dropdown">
-						<a href="store" class="nav-dropdown-btn">Store</a>
+						<a href="store.php" class="nav-dropdown-btn">store</a>
 						<ul class="nav-dropdown-content">
-							<a href="store"><li>All</li></a>
-							<a href="terrariums"><li>Terrariums</li></a>
-							<a href="materials"><li>Materials</li></a>
-							<a href="plants"><li>Plants</li></a>
-							<a href="critters"><li>Critters</li></a>
+							<li><a href="store.php">All</a></li>
+							<li><a href="terrariums.php">Terrariums</a></li> 
+							<li><a href="materials.php">Materials</a></li>
+							<li><a href="plants.php">Plants</a></li>
+							<li><a href="critters.php">Critters</a></li>
 						</ul>
 					</li>
-					<a href="plant-care"><li>Plant Care</li></a>
-					<a href="critter-care"><li>Critter Care,</li></a>
+					<li><a href="PlantCare.php">Plant Care</a></li>
+					<li><a href="CritterCare.php">Critter Care</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
 				</ul>
 			</nav>
 			<nav class="collapse-navbar">
@@ -24,15 +25,16 @@
 			</nav>
 		</div>
 		<ul class="dropdown" id="dropdown">
-			<li><a href="home">Home</a></li>
-			<li onclick="showHideSubNav()" style="cursor: pointer"><a>Store</a></li>
-			<li style="text-align: center;"><a href="plant-care">Plant<br/>Care</a></li>
-			<li style="text-align: center;"><a href="critter-care">Critter<br/>Care</a></li>
+			<li><a href="index.php">Home</a></li>
+			<li onclick="showHideSubNav()" style="cursor: pointer"><a>store</a></li>
+			<li style="text-align: center;"><a href="PlantCare.php">Plant<br/>Care</a></li>
+			<li style="text-align: center;"><a href="CritterCare.php">Critter<br/>Care</a></li>
+	  		<li style="text-align: center;"><a href="contact.php">Contact<br/>Us</a></li>
 		</ul>
 		<ul class="sub-dropdown" id="sub-dropdown">
-			<li><a href="store">All</a></li>
-			<li><a href="terrariums">Terrariums</a></li>
-			<li><a href="materials">Materials</a></li>
-			<li><a href="plants">Plants</a></li>
-			<li><a href="critters">Critters</a></li>
+			<li><a href="store.php">All</a></li>
+			<li><a href="terrariums.php">Terrariums</a></li>
+			<li><a href="materials.php">Materials</a></li>
+			<li><a href="plants.php">Plants</a></li>
+			<li><a href="critters.php">Critters</a></li>
 		</ul>
