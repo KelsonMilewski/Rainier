5f78d92468a8d19116e8405a2fa59604b6d44589
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2020-09-19 18:51:23 -0700

    Update

--- include/head.php
+++ include/head.php
@@ -1,4 +1,4 @@
-		<meta charset="utf-8">
+        <meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="styles.css">
 		<link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
@@ -11,4 +11,21 @@
 			function gtag(){dataLayer.push(arguments);}
 			gtag('js', new Date());
 			gtag('config', 'UA-154593775-1');
+		</script>
+		<script>
+			function showHideNav() {
+				if (document.getElementById("dropdown").style.display === "none") {
+					document.getElementById("dropdown").style.display = "flex";
+				} else {
+					document.getElementById("dropdown").style.display = "none";
+					document.getElementById("sub-dropdown").style.display = "none";
+				}
+			}
+			function showHideSubNav() {
+				if (document.getElementById("sub-dropdown").style.display === "none") {
+					document.getElementById("sub-dropdown").style.display = "initial";
+				} else {
+					document.getElementById("sub-dropdown").style.display = "none";
+				}
+			}
 		</script>
